5a59f3cc2203a2ebb417dd6bb5bfc9f20b159332
Pablodamico76 <pablodamico@hotmail.com>
2020-06-24 22:17:10 -0300

    productsCRUD

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -10,9 +10,82 @@ const productsController = {
         res.render(path.resolve(__dirname, '../views/products/productDetail'), { Title: 'Detalle de producto' });
     },
     crud: function (req, res) {
-        //res.sendFile(path.resolve(__dirname, '../views/users/login.html'));
-        res.render(path.resolve(__dirname, '../views/products/productsCRUD'), { Title: 'Productos' });
+        let productos = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../models/productos.json')))
+        res.render(path.resolve(__dirname, '../views/products/productsCRUD'), { Title: 'Productos', productos: productos });
     },
+    register: function (req, res) {
+        //res.sendFile(path.resolve(__dirname, '../views/users/register.html'));
+        res.render(path.resolve(__dirname, '../views/products/register'), { Title: 'Productos'});
+    },
+    save: function (req, res) {
+        
+        let productosActuales = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../models/productos.json')))
+        let productoUltimo = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../models/productos.json')))
+
+        productoUltimo = productoUltimo.pop();
+
+        let productoNuevo = {
+            id: productoUltimo.id + 1,
+            marca: req.body.marca,
+            modelo: req.body.modelo,
+            descripcion: req.body.descripcion,
+            talle: req.body.talle,
+            color:req.body.color,
+            precio: req.body.precio
+
+        }
+
+
+        productosActuales.push(productoNuevo);
+
+        let productoJSON = JSON.stringify(productosActuales)
+
+        fs.writeFileSync(path.resolve(__dirname, '../models/productos.json'), productoJSON)
+        res.redirect('/products/crud');
+
+    },
+    show: function (req, res) {
+
+        let productosActuales = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../models/productos.json')))
+        let productoId = req.params.id;
+        const productoShow = productosActuales.find(producto => producto.id == productoId);
+        res.render(path.resolve(__dirname, '..', 'views', 'products', 'detail'), { productoShow: productoShow, Title: 'Producto-Visualizar' })
+
+    },
+    edit: function (req, res) {
+
+        let productosActuales = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../models/productos.json')))
+        let productoId = req.params.id;
+        const productoEdit = productosActuales.find(producto => producto.id == productoId);
+        res.render(path.resolve(__dirname, '..', 'views', 'products', 'edit'), { productoEdit: productoEdit, Title: 'Producto-Edición' })
+
+    },
+    update: function (req, res) {
+
+        let productosActuales = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../models/productos.json')))
+        req.body.id = req.params.id;
+        let productoUpdate = productosActuales.map(producto => {    //id nombre descripcion precio imagen
+            if (producto.id == req.body.id) {
+                return producto = req.body;
+            }
+            return producto;
+        });
+        productoJSON = JSON.stringify(productoUpdate, null, 2);
+        fs.writeFileSync(path.resolve(__dirname, '../models/productos.json'), productoJSON);
+        res.redirect('/products/crud');
+
+    } ,
+    delete: function (req, res) {
+
+        let productosActuales = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../models/productos.json')))
+        let productoId = req.params.id;
+        const productosNuevos = productosActuales.filter(producto => producto.id != productoId)
+
+        let productoJSON = JSON.stringify(productosNuevos)
+        fs.writeFileSync(path.resolve(__dirname, '../models/productos.json'), productoJSON)
+        res.redirect('/products/crud');
+    },
+
 
 
 }
--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -9,6 +9,12 @@ const productsController = require(path.resolve(__dirname, '../controllers/produ
 
 
 router.get('/productDetail', productsController.productDetail);
+router.get('/products/register', productsController.register);
 router.get('/products/crud', productsController.crud);
+router.post('/products/crud', productsController.save);
+router.get('/products/detail/:id', productsController.show);
+router.get('/products/delete/:id', productsController.delete);
+router.get('/products/edit/:id', productsController.edit);
+router.put('/products/edit/:id', productsController.update);
 
 module.exports = router;
