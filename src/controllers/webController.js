c4a687b97c327314ba1727acf2170e320bae0356
Mariano <marianomartinezventura@gmail.com>
2020-07-10 00:00:01 -0300

    MM fix error de producto y una ruta

--- src/controllers/webController.js
+++ src/controllers/webController.js
@@ -7,8 +7,9 @@ let { check, validationResult, body } = require('express-validator');
 
 const webController = {
     index: function(req,res){
+        let categories = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../models/categorias.json')));
         //res.sendFile(path.resolve(__dirname, '../views/web/index.html'));
-        res.render(path.resolve(__dirname, '..', 'views', 'web', 'index'), { Title: 'Home', usuario: req.session.usuarioLogueado});
+        res.render(path.resolve(__dirname, '..', 'views', 'web', 'index'), { Title: 'Home', usuario: req.session.usuarioLogueado, categories});
     },
     nosotros: function(req,res){
         //res.sendFile(path.resolve(__dirname, '../views/web/nosotros.html'));
