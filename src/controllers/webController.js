332694cbd585d1456c614cd8a4a85f20aa4d9b08
Mariano <marianomartinezventura@gmail.com>
2020-08-12 17:21:22 -0300

    index.ejs recibe las categorías desde db. Para las categorías de NavbarTop falta algo que envíe categories de forma global a todas las vistas

--- src/controllers/webController.js
+++ src/controllers/webController.js
@@ -1,16 +1,20 @@
 const path = require('path');
 const fs = require('fs');
+const db = require('../database/models/');
+const Category = db.Category;
+
 //Express validator
 let { check, validationResult, body } = require('express-validator');
 
 
-
 const webController = {
     index: function(req,res){
-        let categories = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../models/categorias.json')));
+        // let categories = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../models/categorias.json')));
         //res.sendFile(path.resolve(__dirname, '../views/web/index.html'));
-       
-        res.render(path.resolve(__dirname, '..', 'views', 'web', 'index'), { Title: 'Home', categories});
+        Category.findAll()
+        .then(categories=>{
+            res.render(path.resolve(__dirname, '..', 'views', 'web', 'index'), { Title: 'Home', categories});
+        })
     },
     nosotros: function(req,res){
         //res.sendFile(path.resolve(__dirname, '../views/web/nosotros.html'));
