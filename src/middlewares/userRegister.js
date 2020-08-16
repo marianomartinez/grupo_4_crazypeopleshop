2179264c70c317098f79b0918a3fc4b192a75192
Pablodamico76 <pablodamico@hotmail.com>
2020-08-15 22:07:27 -0300

    validacion en el front user create y register

--- src/middlewares/userRegister.js
+++ src/middlewares/userRegister.js
@@ -11,7 +11,7 @@ module.exports = [
         check('firstName').isLength({min:1}).withMessage('el nombre no puede quedar vacío'),
         check('lastName').isLength({ min: 1 }).withMessage('el apellido no puede quedar vacío'),
         check('email').isEmail().withMessage('el formato del mail es erroneo'),
-        check('password').isLength({ min: 6, max: 15 }).withMessage('la clave debe ser entre 6 y 15 caracteres'),
+        check('password').isLength({ min: 6, max: 15 }).withMessage('la contraseña debe tener entre 6 y 15 caracteres'),
         body('password').custom(function (value, { req }) {
             if (req.body.password2 == value) {
                 return true
